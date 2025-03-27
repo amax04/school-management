6dfc6583526b20037090f7fd04c423ef6eca61ad
amax04 <saxenaam12@gmail.com>
2025-03-27 20:28:35 +0530

    By Aman Updates

---
+++ src/main/java/com/example/school_management/student_feature/Config/WebConfig.java
@@ -0,0 +1,13 @@
+/*
+ * package com.example.school_management.student_feature.Config;
+ * 
+ * import org.springframework.context.annotation.Configuration; import
+ * org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+ * import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+ * 
+ * @Configuration public class WebConfig implements WebMvcConfigurer {
+ * 
+ * @Override public void addResourceHandlers(ResourceHandlerRegistry registry) {
+ * registry.addResourceHandler("/admin/photos/**")
+ * .addResourceLocations("classpath:/uploads/photos/"); } }
+ */
